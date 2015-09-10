3239caa830f8a2d7fb979a52a443c115de12ec3a
Mario Feles dos Santos Junior <mario_feles@live.com>
2015-09-10 11:06:40 -0300

    Update MainActivity.java

--- app/src/main/java/com/mariofeles/appmaterialdesign/activity/MainActivity.java
+++ app/src/main/java/com/mariofeles/appmaterialdesign/activity/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-
         drawerFragment = (FragmentDrawer)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
